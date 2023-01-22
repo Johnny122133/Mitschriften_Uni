9044f3753fbceabc9d42bdf81a1a22ccee7d43b4
Johnny122133 <jonathanmayerbiker@gmail.com>
2023-01-22 12:54:54 +0100

    added footfullcite to Examples_V2

--- MathMeth_Wissenssammlung/compile_tex.cmd
+++
@@ -1,6 +0,0 @@
-pdflatex -draftmode main.tex
-bibtex file :: or biber
-:: makeindex file.idx # if needed
-:: makeindex -s style.gls ...# for glossary if needed
-pdflatex -draftmode main.tex
-pdflatex main.tex
