3482cf37f44c75d2f2d3ca77f082ebbf268eef56
Johnny122133 <jonathanmayerbiker@gmail.com>
2023-01-27 20:33:42 +0100

    updated python package name

--- Modules/setup.py
+++ Modules/setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages, find_namespace_packages
 
 setup(
-    name='Modules',
+    name='modules',
     version='0.1.1',
-    packages=find_packages(include=['Modules']),
-    namespace_packages = find_namespace_packages(include=['Modules'])
+    packages=find_packages(include=['modules']),
+    namespace_packages = find_namespace_packages(include=['modules'])
 )
